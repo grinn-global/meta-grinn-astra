0e9f9f9a867f5071c8fb2f67b29e1ef4cee2ccc4
Lukasz Madej <l.madej@grinn-global.com>
2025-07-09 14:58:37 +0200

    recipes-devtools: synasdk-v4l2isp-daemon: customize isp_media_server
    Adjust isp_media_server parameters so sensor configuration matches
    AstraADA and AstraEVB platforms.

---
+++ recipes-devtools/synasdk/grinn-astra-1680-ada/isp_media_server.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+NAME=isp_media_server
+PATH=/sbin:/bin:/usr/sbin:/usr/bin
+DESC="ISP Media Server"
+
+DAEMON=/usr/bin/$NAME
+
+mkdir -p /run/$NAME
+RUN=/run/$NAME
+
+PIDFILE="$RUN/$NAME.pid"
+
+mkdir -p /var/log
+VAR_LOG="/var/log"
+
+LOGFILE="$VAR_LOG/isp.log"
+
+test -f $DAEMON || exit 0
+
+SSD_OPTIONS="--oknodo --quiet  --make-pidfile --pidfile $PIDFILE  --exec $DAEMON"
+SSD_KILL="--signal KILL --retry 5"
+
+set -e
+
+case $1 in
+    start)
+        echo "sensor=ov5647 xml=/usr/share/OV5647_480p.xml manu_json=/usr/share/ISP_Manual_IMX258.json \
+        auto_json=/usr/share/ISP_Auto.json i2c_bus_id=3 mipi_id=0 mode=0" > /proc/vsi/isp_subdev0
+        echo "1 sensor=ov5647 1 xml=/usr/share/OV5647_480p.xml 1 manu_json=/usr/share/ISP_Manual_IMX258.json \
+        1 auto_json=/usr/share/ISP_Auto.json 1 i2c_bus_id=0 1 mipi_id=1 1 mode=0" > /proc/vsi/isp_subdev0
+        echo -n "Starting $DESC: "
+        start-stop-daemon --start $SSD_OPTIONS  > $LOGFILE &
+        echo "${DAEMON##*/}."
+        ;;
+    stop)
+        echo -n "Stopping $DESC: "
+        start-stop-daemon  --stop $SSD_OPTIONS $SSD_KILL
+        echo "${DAEMON##*/}."
+        ;;
+    restart|force-reload)
+        $0 stop
+        sleep 1
+        $0 start
+        exit 0
+        ;;
+    status)
+        status ${DAEMON} || exit $?
+        ;;
+    *)
+        echo "Usage: $0 {start|stop|restart|force-reload|status}" >&2
+        exit 1
+        ;;
+esac
+
+exit 0
