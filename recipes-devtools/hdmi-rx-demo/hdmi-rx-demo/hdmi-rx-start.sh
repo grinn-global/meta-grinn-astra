7521dbc748a56daefd5dc96c051f94728418e43e
Lukasz Madej <l.madej@grinn-global.com>
2025-08-29 10:35:44 +0200

    recipes-devtools: hdmi-rx-demo: add recipe
    Introduce recipe providing a shell script demonstrating HDMI RX usage
    on the AstraADA platform.

---
+++ recipes-devtools/hdmi-rx-demo/hdmi-rx-demo/hdmi-rx-start.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+export XDG_RUNTIME_DIR=/var/run/user/0
+export WAYLAND_DISPLAY=wayland-1
+
+HDMI_RX_DEV="none"
+
+get_hdmi_rx_device() {
+    eval "$(v4l2-ctl --list-devices | awk '
+        /^synaptics-hrx \(platform: syna-hrx\):$/ {
+            getline
+            if ($1 ~ /^\/dev\/video[0-9]+$/) {
+                print "HDMI_RX_DEV=" $1
+            }
+        }
+    ')"
+}
+
+get_platform() {
+    local HOSTNAME
+    HOSTNAME=$(uname -n)
+
+    case "$HOSTNAME" in
+        *-evb)
+            PLATFORM="EVB"
+            ;;
+        *-ada)
+            PLATFORM="ADA"
+            ;;
+        *)
+            PLATFORM="none"
+            ;;
+    esac
+}
+
+start_pipeline() {
+    if [ "$HDMI_RX_DEV" = "none" ]; then
+        echo "HDMI RX device not available - cannot start pipeline"
+        exit 1
+    fi
+
+    gst-launch-1.0 v4l2src device=$HDMI_RX_DEV ! video/x-raw,width=1920,height=1080,fps=30,format=NV12 ! waylandsink fullscreen=true
+}
+
+get_hdmi_rx_device
+get_platform
+
+echo "PLATFORM=$PLATFORM"
+
+if [ "$PLATFORM" != "ADA" ]; then
+    echo "Only the ADA platform is supported!"
+    exit 1
+fi
+
+echo "HDMI_RX_DEV=$HDMI_RX_DEV"
+
+start_pipeline
+
