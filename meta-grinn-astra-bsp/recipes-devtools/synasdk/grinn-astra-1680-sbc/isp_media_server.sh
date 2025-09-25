33c689717a71b877caff8f04ae92a908721d7805
Lukasz Madej <l.madej@grinn-global.com>
2025-09-25 15:34:40 +0200

    bsp: recipes-devtools: synasdk-v4l2isp-daemon: add AstraSBC configuration
    Add isp_media_server configuration for AstraSBC board so it uses single
    OV5647 image sensor with 2k resolution.

---
+++ meta-grinn-astra-bsp/recipes-devtools/synasdk/grinn-astra-1680-sbc/isp_media_server.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+NAME=isp_media_server
+PATH=/sbin:/bin:/usr/sbin:/usr/bin
+DESC="ISP Media Server"
+
+DAEMON=/usr/bin/$NAME
+
+mkdir -p /run/$NAME
+RUN=/run/$NAME
+
+PIDFILE="$RUN/$NAME.pid"
+
+mkdir -p /var/log
+VAR_LOG="/var/log"
+
+LOGFILE="$VAR_LOG/isp.log"
+
+test -f $DAEMON || exit 0
+
+SSD_OPTIONS="--oknodo --quiet  --make-pidfile --pidfile $PIDFILE  --exec $DAEMON"
+SSD_KILL="--signal KILL --retry 5"
+
+set -e
+
+case $1 in
+    start)
+        echo "sensor=ov5647 xml=/usr/share/OV5647_2k.xml manu_json=/usr/share/ISP_Manual_OV5647_2k.json \
+        auto_json=/usr/share/ISP_Auto_OV5647_2k.json i2c_bus_id=0 mipi_id=0 mode=1 csi_clock_rate=300000000" > /proc/vsi/isp_subdev0
+        echo -n "Starting $DESC: "
+        start-stop-daemon --start $SSD_OPTIONS  > $LOGFILE &
+        echo "${DAEMON##*/}."
+        ;;
+    stop)
+        echo -n "Stopping $DESC: "
+        start-stop-daemon  --stop $SSD_OPTIONS $SSD_KILL
+        echo "${DAEMON##*/}."
+        ;;
+    restart|force-reload)
+        $0 stop
+        sleep 1
+        $0 start
+        exit 0
+        ;;
+    status)
+        status ${DAEMON} || exit $?
+        ;;
+    *)
+        echo "Usage: $0 {start|stop|restart|force-reload|status}" >&2
+        exit 1
+        ;;
+esac
+
+exit 0
